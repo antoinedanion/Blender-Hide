ce7398cdbfc98ddc2cfc8e96e81a48ea2b8d5e9c
Antoine Danion <antoinedanion.pro@gmail.com>
2024-11-14 00:40:13 +0100

    fix: upgraded to new preferences path api

--- constants.py
+++ constants.py
@@ -6,8 +6,10 @@
 
 import os
 
+import bpy
+
 ADDON_NAME = __package__
 OP_IDNAME_PREFIX = ADDON_NAME.lower().split('.')[-1]
 
-PREFS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'prefs')
+PREFS_DIR = bpy.utils.extension_path_user(ADDON_NAME, path="prefs", create=True)
 PREFS_FILEPATH = os.path.join(PREFS_DIR, 'hide_prefs.json')
